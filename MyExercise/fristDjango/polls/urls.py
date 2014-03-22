dc9a77f70044316b4c0a754c9db59c83fe4e687d
kolaman <wpf420@gmail>
2014-03-22 13:30:36 +0800

    update

--- MyExercise/fristDjango/polls/urls.py
+++ MyExercise/fristDjango/polls/urls.py
@@ -4,7 +4,8 @@ from polls import views
 urlpatterns = patterns('',
 	#ex:/polls/
 	url(r'^$',views.index,name='index'),
-	url(r'^(?P<poll_id>\d+)/$',views.detail,name='detail'),
+	url(r'^(?P<poll_id>\d+)/$',views.detail),
+	url(r'^test/(?P<poll_id>\d+)/$',views.detail,name='detail'),
 	url(r'^(?P<poll_id>\d+)/results/$',views.results,name='result'),
 	url(r'^(?P<poll_id>\d+)/vote/$',views.vote,name='vote'),
 	)
--- MyExercise/fristDjango/polls/views.py
+++ MyExercise/fristDjango/polls/views.py
@@ -1,15 +1,22 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from models import Poll
+#from django.template import Context,loader
+from django.shortcuts import render
 
 def index(request):
-	#return HttpResponse("Hello,world,you'are at the polls index.")
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	output = ';</br>'.join([p.question for p in latest_poll_list])
-	return HttpResponse(output)
+	context = {'latest_poll_list':latest_poll_list}
+	return render(request,'polls/index.html',context)
 
 def detail(request,poll_id):
-	return HttpResponse("You're looking at the poll %s."%poll_id)
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		#return render(request,'404.html')	
+		raise Http404
+	return render(request,'polls/detail.html',{"poll":poll})	
+
 def results(request,poll_id):
 	return HttpResponse("You're looking at the results of poll %s."%poll_id) 
 def vote(request,poll_id):
